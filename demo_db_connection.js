f439072e88022a9809c92317dd58008401a6409c
bright0220 <jackfortin0220@gmail.com>
2022-12-06 00:01:19 -0500

    insert_db

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -11,9 +11,9 @@ con.connect(function (err) {
   if (err) throw err;
   console.log("Connected");
   var sql =
-    "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))";
+    "INSERT INTO  customers (name, address) VALUES ('Company Inc', 'Highway 37')";
   con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Table created");
+    console.log("1 record inserted");
   });
 });
