e07f194ab039baff187e47df077b68bcb96513f4
bright0220 <jackfortin0220@gmail.com>
2022-12-10 20:17:08 -0500

    mongodb limit

---
+++ demo_mongodb_limit.js
@@ -0,0 +1,16 @@
+var MongoClient = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function (err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  dbo
+    .collection("customers")
+    .find()
+    .limit(5)
+    .toArray(function (err, res) {
+      if (err) throw err;
+      console.log(res);
+      db.close();
+    });
+});
