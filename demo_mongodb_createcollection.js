281f66c55a09d326d1b4a00686ea289b7f1157b7
bright0220 <jackfortin0220@gmail.com>
2022-12-10 20:31:11 -0500

    mongodb join

--- demo_mongodb_createcollection.js
+++ demo_mongodb_createcollection.js
@@ -1,12 +1,12 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017/';
+var MongoClient = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27017/";
 
-MongoClient.connect(url, function(err, db) {
-    if(err) throw err;
-    var dbo = db.db('mydb');
-    dbo.createCollection('customers', function(err, res) {
-        if(err) throw err;
-        console.log('Collection created!', res);
-        db.close();
-    });
+MongoClient.connect(url, function (err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  dbo.createCollection("orders", function (err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  });
 });
--- demo_mongodb_insert_id.js
+++ demo_mongodb_insert_id.js
@@ -4,12 +4,8 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function (err, db) {
   if (err) throw err;
   var dbo = db.db("mydb");
-  var myobj = [
-    { _id: 154, name: "Chocolate Heaven" },
-    { _id: 155, name: "Tasty Lemon" },
-    { _id: 156, name: "Vanilla Dream" },
-  ];
-  dbo.collection("products").insertMany(myobj, function (err, result) {
+  var myobj = { _id: 1, product_id: 154, status: 1 };
+  dbo.collection("orders").insertOne(myobj, function (err, result) {
     if (err) throw err;
     console.log(result);
     db.close();
